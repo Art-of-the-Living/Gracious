f8d824a84e8dca4d3443021f5c586bdb41cfa649
Kenneth J. Grace <kennethjeffersongrace@gmail.com>
2022-04-18 03:01:58 -0700

    qualia and synapse documentations

--- base/synapse.go
+++ base/synapse.go
@@ -1,11 +1,16 @@
 package base
 
+// The Synapse performs the crucial job of connecting associations to neuron groups. Each synapse has a certain weight
+// which is either negative 1 or positive one. Once the weight has been set to one via +3:-1 Hebbian learning, the weight
+// must be reset by a raise in the correlation sum threshold value.
 type Synapse struct {
 	// Internal Attributes
 	correlationSum int
 	weightValue    int
 }
 
+// NewSynapse initializes a new Synapse with a -1 weight value and a 0 correlation sum. A pointer to the Synapse is
+// returned.
 func NewSynapse() Synapse {
 	syn := Synapse{weightValue: -1, correlationSum: 0}
 	return syn
@@ -17,12 +22,12 @@ func NewSynapse() Synapse {
 //performs +3:-1 incremental steps.
 func (syn *Synapse) Evoke(training int, association int, correlation int, learningControl int) int {
 	if learningControl > 0 {
-		if syn.weightValue != 1 {
+		if syn.correlationSum > correlation {
 			syn.correlationSum += 4 * association * training
 			syn.correlationSum -= association
-			if syn.correlationSum > correlation {
-				syn.weightValue = 1
-			}
+			syn.weightValue = 1
+		} else {
+			syn.weightValue = -1
 		}
 	}
 	return association * syn.weightValue
--- model/qualia.go
+++ model/qualia.go
@@ -6,46 +6,53 @@ import (
 	"strings"
 )
 
+// An Address is the identifying tag for a location of an object in the qualar system. Since quale have a structure
+// the Address serves to add that structure.
 type Address struct {
 	X int
 	Y int
 }
 
+// vectorized returns a slice representation of the Address
 func (a Address) vectorized() []int {
 	return []int{a.X, a.Y}
 }
 
+// A Quale is the smallest Gracious unit of information. Once imported into the system it becomes the fundamental unit
+// of subjective experience. A Quale has a dynamic structure across different Qualar types, such as vision and audition.
+// At its core a Quale is just a potentially VERY large pattern of values.
 type Quale struct {
 	features map[Address]int
 }
 
+// NewQuale returns a new Quale initialized with no features set. The Quale's size at this point is 0,
+// and it occupies very little space in the system. As features are set more, more will populate the quale. Zero values
+// will never be set, as the 0 is assumed by the absence of a feature.
 func NewQuale() Quale {
 	return Quale{features: make(map[Address]int)}
 }
 
-func (q *Quale) Zero() {
-	for addr := range q.features {
-		q.features[addr] = 0
-	}
-}
-
+// Size returns the total number of present features in the quale. It says nothing about any MAX Size.
 func (q *Quale) Size() int {
 	return len(q.features)
 }
 
-// SetFeature sets the feature value at a given line of the Quale. If there is no feature available at a given line,
-// then SetFeature returns an error.
+// SetFeature sets the feature value at a given address of the Quale. If there is no feature available at any given
+// address, then SetFeature returns an error.
 func (q *Quale) SetFeature(address Address, strength int) error {
 	if _, ok := q.features[address]; ok {
-		q.features[address] = strength
+		if strength != 0 {
+			q.features[address] = strength
+			return nil
+		}
 		return nil
 	} else {
-		q.features[address] = strength
 		return errors.New(fmt.Sprint("Quale is located at", &q,
 			"Bad access! ", address.vectorized(), "was write accessed"))
 	}
 }
 
+// SetFeatures sets all the features of a quale equal to all the values at corresponding addresses.
 func (q *Quale) SetFeatures(array map[Address]int) {
 	q.features = make(map[Address]int)
 	for address, value := range array {
@@ -53,6 +60,8 @@ func (q *Quale) SetFeatures(array map[Address]int) {
 	}
 }
 
+// SetQuale sets the values of one quale to all the value of another without clearing the old values of the original
+// Quale. The function should be accompanied by a call to Quale.Zero.
 func (q *Quale) SetQuale(instantaneousQ Quale) {
 	q.SetFeatures(instantaneousQ.GetFeatures())
 }
@@ -68,16 +77,32 @@ func (q *Quale) GetFeature(address Address) (int, error) {
 	}
 }
 
-// GetFeatures returns the complete array of features. This function should be used sparingly as it does not test
-// for length inequalities and trouble can arise when manually mapping the features array to signal lines.
+// GetFeatures returns the complete map of features.
 func (q *Quale) GetFeatures() map[Address]int {
 	return q.features
 }
 
+// AdjustFeature adjusts a feature by a given value up or down by addition or subtraction. In the event that the
+// resulting value is returned to 0, the feature is removed from the Quale.
 func (q *Quale) AdjustFeature(address Address, value int) {
-	q.features[address] = q.features[address] + value
+	newFeature := q.features[address] + value
+	if newFeature != 0 {
+		q.features[address] = newFeature
+	} else {
+		delete(q.features, address)
+	}
 }
 
+// Clear removes every feature from the quale. Effectively resetting all values to 0 and reducing the size of the quale.
+// This should be called to avoid any overwriting.
+func (q *Quale) Clear() {
+	for addr := range q.features {
+		delete(q.features, addr)
+	}
+}
+
+// Strength returns the sum of all the features present in the quale. This is most often used for importance comparisons
+// to other Quale.
 func (q *Quale) Strength() int {
 	sum := 0
 	for _, feature := range q.features {
@@ -86,6 +111,8 @@ func (q *Quale) Strength() int {
 	return sum
 }
 
+// Decay is essential to releasing quale from dispatch. Over time a quale which has secured the dispatch system should
+// slowly be decremented until another quale takes control. High values will not lower below 1.
 func (q *Quale) Decay() {
 	for address, feature := range q.features {
 		if feature > 1 {
@@ -94,6 +121,8 @@ func (q *Quale) Decay() {
 	}
 }
 
+// Represent provides a method fpr printing the contents of the quale. A string value is returned which represents all
+// the features in the quale at that time.
 func (q *Quale) Represent() string {
 	featureRepresentations := make([]string, 0)
 	for address, feature := range q.features {
