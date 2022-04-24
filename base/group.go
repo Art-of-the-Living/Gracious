9645573fca9bbbf3e3065dbe00b26b2e83223efb
Kenneth J. Grace <kennethjeffersongrace@gmail.com>
2022-04-23 23:10:08 -0700

    complete refactor

--- base/group.go
+++ base/group.go
@@ -1,18 +1,14 @@
 package base
 
-import (
-	"github.com/KennethGrace/gracious/model"
-)
-
 // Group is a set of neurons with a specific associative quale type input and a specific main quale type input and output.
 type Group struct {
 	// Internal Attributes
 	binding string // The name of the system this neuron group is a part of.
-	neurons map[model.Address]Neuron
+	neurons map[Address]Neuron
 	// Inbound Attributes
 	CorrelationThresholdSignal int
 	LearningControlSignal      int
-	Association                model.Quale
+	Association                chan Quale
 	PassThrough                bool
 	// Outbound Attributes
 	Match   int
@@ -20,29 +16,28 @@ type Group struct {
 }
 
 func NewGroup(binding string) *Group {
-	neurons := make(map[model.Address]Neuron)
+	neurons := make(map[Address]Neuron)
 	ng := Group{neurons: neurons, binding: binding, LearningControlSignal: 1}
 	return &ng
 }
 
-func (g *Group) SetAssociation(a model.Quale) {
-	g.Association = a
-}
-
-// Evoke updates the Neuron Group for the moment of time, T.
-func (g *Group) Evoke(main model.Quale) model.Quale {
-	newQuale := model.NewQuale()
+func (g *Group) Evoke(main Quale, out chan Quale) {
+	association := NewQuale()
+	select {
+	case association = <-g.Association:
+	}
+	newQuale := NewQuale()
 	if g.PassThrough {
 		newQuale = main
 	}
 	for featureAddress, feature := range main.GetFeatures() {
 		if neuron, ok := g.neurons[featureAddress]; ok {
-			sum := neuron.Evoke(feature, g.Association, g.CorrelationThresholdSignal, g.LearningControlSignal)
+			sum := neuron.Evoke(feature, association, g.CorrelationThresholdSignal, g.LearningControlSignal)
 			newQuale.AdjustFeature(featureAddress, sum)
 		} else {
 			g.neurons[featureAddress] = NewNeuron()
 		}
 	}
 	newQuale.WinnersTakeAll(0)
-	return newQuale
+	out <- newQuale
 }
